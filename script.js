73bb771f432ce000e8f9a580d84850b77207a511
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2016-08-07 19:15:18 -0400

    blur on scroll is working!

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	var height = $(window).height(); // gets the height of the window
+
+	initialAdjustments();
+
+	function initialAdjustments() {
+		$("div#body").css({"margin-top": height - 80 + "px"}); // adjusts the white box's margin to be below the window's height
+	}
+
+
+	$(document).scroll(function() {
+		var scrollTop = $(this).scrollTop();
+		var pixels = scrollTop / 70;
+
+		if (scrollTop < height) {
+			$("div.container-fluid").css({
+				"-webkit-filter": "blur(" + pixels + "px)",
+				"background-position": "center -" + pixels * 10 + "px"
+			});
+		}
+
+	});
+
+});
